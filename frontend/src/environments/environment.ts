a80ecf3816b19ca64fda35b905908751c7a413e7
Julio Lugo <juliocesarfy6@gmail.com>
2026-05-25 00:21:45 -0700

    correccion de variable local4

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    // Cambia esta URL cuando tengas tu backend desplegado.
-    // Ejemplo: 'https://medrecord-api.onrender.com/api'
-    apiUrl: 'https://TU-BACKEND-PUBLICO/api'
+    apiUrl: 'https://medrecord-api.onrender.com/api'
 };
